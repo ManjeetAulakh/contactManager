a134b704c3d7184d6fb13ea84c74dca0156ea4be
Manjeet Singh <118804835+ManjeetAulakh@users.noreply.github.com>
2025-07-01 17:11:59 +0530

    "Initial commit"

---
+++ src/main/java/com/scm/contactManager/controllers/PageController.java
@@ -0,0 +1,32 @@
+package com.scm.contactManager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String hello(Model model){
+
+        // sending data to view
+        model.addAttribute("name", "Spring Tech");
+        model.addAttribute("youtube", "Inside India");
+        model.addAttribute("github", "htpsss");
+        
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String service() {
+        return "services";
+    }
+    
+    
+}
